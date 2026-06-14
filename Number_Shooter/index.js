bb34097aad219028da07040211f270e367990fcb
ori-Balajee <balajee4drip@gmail.com>
2026-06-14 12:34:51 +0530

    Number Shooter

---
+++ Number_Shooter/index.js
@@ -0,0 +1,69 @@
+let numberContainer = document.querySelector('.numberContainer');
+let timerValue = document.querySelector('.timerValue');
+let targetValue = document.querySelector('.targetValue');
+let scoreValue = document.querySelector('.scoreValue');
+let numberOfCircles = 80;
+let timer = 5;
+let target;
+
+restartGame();
+startTimer();
+
+function restartGame() {
+    timerReset();
+    generateTarget();
+    generateNumbers();
+    scoreValue.innerText = 0;
+}
+
+function timerReset(){
+    timerValue.innerText = timer;
+    timer = 5;
+}
+
+function generateNumbers() {
+    numberContainer.innerHTML = ''
+    for (let i = 1; i <= numberOfCircles; i++) {
+
+        let divElem = document.createElement('div')
+        divElem.className = 'circle';
+        let number = Math.ceil(Math.random() * 10)
+        divElem.innerText = number;
+
+        numberContainer.append(divElem)
+    }
+}
+
+function generateTarget(){
+    let number = Math.ceil(Math.random()*10);
+    targetValue.innerText = number;
+}
+
+function startTimer() {
+    setInterval(() => {
+        if (timer <= 0) {
+            numberContainer.innerHTML = `
+                <div>Game Over 
+                <button onclick=restartGame()>Reset Game</button>
+                </div>
+                `;
+            return;
+        }
+        timer--;
+        timerValue.innerText = timer;
+    }, 1000);
+}
+
+
+numberContainer.addEventListener('click', function(event){
+    if(event.target.className === 'circle'){
+        let number = Number(event.target.innerText);
+        let targetNumber = Number(targetValue.innerText);
+        if(targetNumber === number){
+            let sv = Number(scoreValue.innerText);
+            sv += 10;
+            scoreValue.innerText = sv;
+        }
+        generateTarget();
+    }
+})
